712afbd112138000c9e200b650bd99c1d3aa696b
Rawan <Rawan.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-10-12 14:52:24 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+
+
+const val APPLES= 50
+fun main(args: Array<String>) {
+
+
+    val numberofDozens= APPLES / 12
+    val remaindofApples=  APPLES % 12
+
+
+        println(numberofDozens)
+       println(remaindofApples)
+
+
+
+
+
+}
+
+
+
+
